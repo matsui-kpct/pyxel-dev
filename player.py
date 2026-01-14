2b37cca720a7be7a2610f9f6562a7a7776493f12
yohei MATSUI <matsui@kumamoto-pct.ac.jp>
2026-01-14 16:45:13 +0900

    ちくわゲームのデモ（未完成）

---
+++ player.py
@@ -0,0 +1,22 @@
+"""
+プレイヤーキャラクターを管理するクラス。
+左右移動と描画を担当する。
+"""
+import pyxel
+
+class Player():
+    def __init__(self):
+        """プレイヤーを初期位置（画面下寄せ）で生成する。"""
+        self.x = 80
+        self.y = 128 - 24
+    
+    def update(self):
+        """左右キー入力を受けてプレイヤーを水平方向に移動する。"""
+        if pyxel.btn(pyxel.KEY_LEFT) and self.x > 0:
+            self.x -= 2
+        if pyxel.btn(pyxel.KEY_RIGHT) and self.x < 128 - 14:
+            self.x += 2
+    
+    def draw(self):
+        """現在の位置にプレイヤーのスプライトを描画する。"""
+        pyxel.blt(self.x, self.y-8, 0, 16, 0, 16, 16, 0)
